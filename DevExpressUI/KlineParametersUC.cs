9242c3d20f39615ac6ec880ec2d7eab0d7ed101f
barisbayin@gmail.com <barisbayin@gmail.com>
2022-03-02 09:47:17 +0300

    12

--- DevExpressUI/KlineParametersUC.cs
+++ DevExpressUI/KlineParametersUC.cs
@@ -1,20 +1,8 @@
 ﻿using Business.Abstract;
-using Business.Concrete;
 using Business.DependencyResolvers;
-using DataAccess.Abstract;
-using DevExpress.XtraEditors;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using Core.Costants.Messages;
 using Entity.Concrete.Entities;
+using System;
 
 namespace DevExpressUI
 {
