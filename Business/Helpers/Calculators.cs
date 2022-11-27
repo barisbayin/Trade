40862af1b50d5ae45a02adc6f28f29f3cfc5c914
BARIS-PC2022\BARIS <barisbayin@gmail.com>
2022-11-27 20:11:43 +0300

    Refactoring calculator methods

--- Business/Helpers/Calculators.cs
+++ Business/Helpers/Calculators.cs
@@ -35,112 +35,25 @@ namespace Business.Helpers
 
         public async Task<IDataResult<int>> CalculateKlineAmountByInterval(string klineInterval, int dayParameter)
         {
-            int klineAmount = 0;
-
-            if (klineInterval == "OneMonth")
+            int klineAmount = klineInterval switch
             {
-
-                klineAmount = Convert.ToInt32(dayParameter / 120);
-
-            }
-
-            if (klineInterval == "OneWeek")
-            {
-
-                klineAmount = Convert.ToInt32(dayParameter / 7);
-
-            }
-
-            if (klineInterval == "ThreeDay")
-            {
-
-                klineAmount = Convert.ToInt32(dayParameter / 3);
-
-            }
-
-            if (klineInterval == "OneDay")
-            {
-
-                klineAmount = dayParameter;
-
-            }
-
-            if (klineInterval == "TwelveHour")
-            {
-
-                klineAmount = dayParameter * 2;
-
-            }
-
-            if (klineInterval == "EightHour")
-            {
-
-                klineAmount = dayParameter * 3;
-
-            }
-
-            if (klineInterval == "SixHour")
-            {
-
-                klineAmount = dayParameter * 4;
-
-            }
-
-            if (klineInterval == "FourHour")
-            {
-
-                klineAmount = dayParameter * 6;
-
-            }
-
-            if (klineInterval == "TwoHour")
-            {
-
-                klineAmount = dayParameter * 12;
-
-            }
-
-            if (klineInterval == "OneHour")
-            {
-
-                klineAmount = dayParameter * 24;
-
-            }
-
-            if (klineInterval == "ThirtyMinutes")
-            {
-
-                klineAmount = dayParameter * 48;
-
-            }
-
-            if (klineInterval == "FifteenMinutes")
-            {
-
-                klineAmount = dayParameter * 96;
-
-            }
-
-            if (klineInterval == "FiveMinutes")
-            {
-
-                klineAmount = dayParameter * 288;
-
-            }
-
-            if (klineInterval == "ThreeMinutes")
-            {
-
-                klineAmount = dayParameter * 480;
-
-            }
-
-            if (klineInterval == "OneMinute")
-            {
-
-                klineAmount = dayParameter * 1440;
-
-            }
+                "OneMonth" => Convert.ToInt32(dayParameter / 120),
+                "OneWeek" => Convert.ToInt32(dayParameter / 7),
+                "ThreeDay" => Convert.ToInt32(dayParameter / 3),
+                "OneDay" => dayParameter,
+                "TwelveHour" => dayParameter * 2,
+                "EightHour" => dayParameter * 3,
+                "SixHour" => dayParameter * 4,
+                "FourHour" => dayParameter * 6,
+                "TwoHour" => dayParameter * 12,
+                "OneHour" => dayParameter * 24,
+                "ThirtyMinutes" => dayParameter * 48,
+                "FifteenMinutes" => dayParameter * 96,
+                "FiveMinutes" => dayParameter * 288,
+                "ThreeMinutes" => dayParameter * 480,
+                "OneMinute" => dayParameter * 1440,
+                _ => 0
+            };
 
             return new SuccessDataResult<int>(klineAmount);
         }
